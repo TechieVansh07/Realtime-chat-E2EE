1293b61e314db6d07216307552d46a30b9d2a428
TechieVansh07 <vansh.bhardwaj761@gmail.com>
2026-08-19 00:21:24 +0530

    Initial commit

---
+++ backend/nodeServer/index.js
@@ -0,0 +1,33 @@
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: "http://127.0.0.1:5500",
+        methods: ["GET", "POST"]
+    }
+});
+
+const users = {};
+
+io.on('connection', socket => {
+
+    console.log("A user connected");
+
+    socket.on('new-user-joined', name => {
+        console.log("New user joined:", name);
+
+        users[socket.id] = name;
+
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', {
+            message: message,
+            name: users[socket.id]
+        });
+    });
+    socket.on('disconnect', () => {
+    socket.broadcast.emit('left', users[socket.id]);
+    delete users[socket.id];
+});
+
+});
